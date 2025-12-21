2e593730ea79a0abc6d2f0492778fefcdda43858
hanu0745 <bhanumanthu450@gmail.com>
2025-12-21 16:17:18 +0530

    feat: add CORS middleware and update package dependencies

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 import express from 'express';
+import cors from 'cors';
 import studentRouter from './routers/studentsRouters.js';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/', studentRouter);
 app.get('/users', (req, res) => {
