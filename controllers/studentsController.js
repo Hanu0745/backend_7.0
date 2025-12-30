224c30c3314f187d44b11c5b123c1bd25e65f6dc
hanu0745 <bhanumanthu450@gmail.com>
2025-12-30 11:55:37 +0530

    PUT method

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -46,4 +46,30 @@ const getStudentsDetailsWithFilters = async(req, res) => {
     }
 
 }
-export {getStudentsDetails, addStudents, getStudentById, getStudentsDetailsWithFilters};
+const updateStudents = async(req, res) => {
+    try{
+        const {id} = req.params;
+        const data = req.body;
+        // const updatedData = await student.findOneAndUpdate({stdRoll:id},data);
+        const updatedData = await student.findByIdAndUpdate({_id: id}, data, {new:true})
+        if(!updatedData){
+            return res.status(404).json({message: "user not existed"});
+        }
+        res.status(200).json({student: updatedData, message: "student Updated"})
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+
+}
+const updateStudentsStatus = async (req, res) => {
+    try{
+        await student.updateMany(
+            {status: false},
+            {$set: {status: true}}
+        );
+        res.status(200).json({message: "status updated"});
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
+export {getStudentsDetails, addStudents, getStudentById, getStudentsDetailsWithFilters, updateStudents, updateStudentsStatus};
